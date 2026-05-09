49f5b21fe564b842ab18ec94586c53ca96e24114
nicolascabanas <nicolascabanas13@gmail.com>
2026-05-08 22:04:50 -0300

    feat: implementa classe base Sala conforme diagrama

---
+++ src/Models/Sala.cs
@@ -0,0 +1,12 @@
+namespace Study_Classes_Booking_System.src.Models
+{
+    public abstract class Sala
+    {
+        public string Id { get; set; }
+        public int Capacidade { get; set; }
+        public string Localizacao { get; set; }
+
+        public abstract string GetNome();
+        public abstract int GetCapacidade();
+    }
+}
