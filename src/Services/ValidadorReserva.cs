5764a2ebc7bf7ee69b4e2098a638bdf5f8c9d747
jvmgomes <joao@mgomes.net>
2026-05-09 18:28:41 -0300

    feat: serviço para função de validação da politica

---
+++ src/Services/ValidadorReserva.cs
@@ -0,0 +1,19 @@
+using System;
+using Study_Classes_Booking_System.src.Repositories;
+
+namespace Study_Classes_Booking_System.src.Services
+{
+    public class ValidadorReserva
+    {
+        private IPoliticaReserva _politica;
+
+        public ValidadorReserva(IPoliticaReserva politica) => _politica = politica;
+        //Injetando Dependência
+
+        public void SetPolitica(IPoliticaReserva politica) => _politica = politica;
+
+        public bool Validar(Reserva nova) =>
+            _politica.Validar(nova, ReservaRepository.GetInstance().ListarTodas());
+            //Usando a função de validação em Strategies/IPoliticaReserva, tipo passado externamente
+    }
+}
