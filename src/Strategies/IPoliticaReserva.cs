9127bca03a0736fc8743d23898ae9f5af273d355
jvmgomes <joao@mgomes.net>
2026-05-10 19:38:40 -0300

    feat: prioridade do docente, estratégia

--- src/Strategies/IPoliticaReserva.cs
+++ src/Strategies/IPoliticaReserva.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Study_Classes_Booking_System.src.Strategies
 {
-    public interface IPoliticaReserva
+    public interface IPoliticaReserva //Interface da Estratégia
     {
         bool Validar(Reserva nova, List<Reserva> existentes); // Forma de validar diferentes tipos de reserva
     }
